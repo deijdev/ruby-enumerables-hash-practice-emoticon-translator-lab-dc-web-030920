2bbfc4c3bb948cfdd3c93e84204cca243c41b249
Deijah Price <deijahwilson1@gmail.com>
2020-02-28 06:38:07 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -22,9 +22,9 @@ end
 
 def get_japanese_emoticon(emoticon_file, emoticon)
   emoticon_lib = load_library(emoticon_file)
-  japanese_emoticon = emoticon_lib['get_emoticon'][emoticon]
-  japanese_emoticon ? japanese_emoticon : 'Sorry, that emoticon was not found'
-end
+ japanese_emoticon = emoticon_lib['get_emoticon'][emoticon]
+ japanese_emoticon ? japanese_emoticon : 'Sorry, that emoticon was not found'
+end	
 
 
 def	 get_english_meaning(emoticon_file, emoticon)
